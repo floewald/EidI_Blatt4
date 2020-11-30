8edcb14760f6f90f9f6e6bd86a12c9a5315476b7
fewald <uk029985@student.uni-kassel.de>
2020-11-30 13:31:51 +0100

    added suplist task 2a

--- aufgabe_2.py
+++ aufgabe_2.py
@@ -2,19 +2,21 @@
 # Arthur Flaum
 # Florian Ewald, 333 068 94
 def intSuperliste(suplist):
-    if type(suplist) != int:
+    if type(suplist) != list:
         if type(suplist) == int:
             return True
         else:
             return False
     else:
-        return intSuperliste(suplist)
+        ini = True
+        for m in suplist:
+            isInt = intSuperliste(m) 
+            if not(isInt):
+                return False
+            else:
+                ini = ini and isInt
+        return ini
 
-L = [[1,2,3,4,5], ["f",4,5], ["f", "5"]]
+L = [[1,2,3,4,5], [22,4,5], [1,5]]
 
-# print(intSuperliste(L))
-n = -1
-if type(n)!=int:
-    raise TypeError("Not an Integer")
-if n<0:
-    raise ValueError("Must be positiv")
+print(intSuperliste(L))
